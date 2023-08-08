63ea8b9880f3ce1a56c4fddde52773be0103810b
mattysteves <m.c.stevens74@gmail.com>
2023-08-08 15:41:22 +1200

    Initial commit, with empty main activity

---
+++ app/src/main/java/com/softeng306team15/plantoid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.softeng306team15.plantoid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
