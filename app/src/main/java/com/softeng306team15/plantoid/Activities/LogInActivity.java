8ab629f4672c55a97c6cdac1f4dfd3bf5d85a7f1
rhun558 <rhun558@aucklanduni.ac.nz>
2023-08-15 01:32:19 +1200

    Added login activity to android manifest. Had to swap main and login there to test functionality. Started functionality on signing in. It can now check if user credentials are correct.

--- app/src/main/java/com/softeng306team15/plantoid/Activities/LogInActivity.java
+++ app/src/main/java/com/softeng306team15/plantoid/Activities/LogInActivity.java
@@ -1,13 +1,23 @@
 package com.softeng306team15.plantoid.Activities;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.softeng306team15.plantoid.R;
 
 public class LogInActivity extends AppCompatActivity {
@@ -39,10 +49,33 @@ public class LogInActivity extends AppCompatActivity {
 
         vh = new ViewHolder();
 
+        vh.btnSignIn.setOnClickListener(this::onSignIn);
+
     }
 
     public void onSignIn(View v) {
 
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("users")
+                .whereEqualTo("userName", vh.enterUsername.getText().toString())
+                .whereEqualTo("password", vh.enterPassword.getText().toString())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                Intent mainIntent = new Intent(getBaseContext(), MainActivity.class);
+                                mainIntent.putExtra("User", document.getId());
+                                startActivity(mainIntent);
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 
     public void goCreateAccount(View v) {
