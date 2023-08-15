a890725e6551cd8bae7d67045806eb3d7d174721
mattysteves <m.c.stevens74@gmail.com>
2023-08-15 12:55:21 +1200

    Added item layout integration and resources to item adapter class

--- app/src/main/java/com/softeng306team15/plantoid/Adaptors/ItemAdaptor.java
+++ app/src/main/java/com/softeng306team15/plantoid/Adaptors/ItemAdaptor.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -24,7 +25,11 @@ import com.softeng306team15.plantoid.Models.SeedSeedlingItem;
 import com.softeng306team15.plantoid.R;
 import com.squareup.picasso.Picasso;
 
+import org.w3c.dom.Text;
+
 import java.util.List;
+
+import kotlin.reflect.KVisibility;
 
 public class ItemAdaptor extends RecyclerView.Adapter<ItemAdaptor.ViewHolder> {
 
@@ -55,8 +60,9 @@ public class ItemAdaptor extends RecyclerView.Adapter<ItemAdaptor.ViewHolder> {
         /*
         This View Holder is used as a basis for category items, Plant Care and Decor uses this directly
          */
-        ImageView bestSellerImageView;
-        ImageView newItemImageView;
+        RelativeLayout globalTagLayout;
+        ImageView globalTagImageView;
+        TextView globalTagTextView;
 
         public CategoryViewHolder(View currentItemView){
             super(currentItemView);
@@ -67,8 +73,9 @@ public class ItemAdaptor extends RecyclerView.Adapter<ItemAdaptor.ViewHolder> {
     }
 
     private class PlantAndTreeViewHolder extends CategoryViewHolder{
-
+        RelativeLayout subCategoryLayout;
         TextView subCategoryTextView;
+        ImageView subCategoryImageView;
 
         private PlantAndTreeViewHolder(View currentItemView){
             super(currentItemView);
@@ -78,8 +85,11 @@ public class ItemAdaptor extends RecyclerView.Adapter<ItemAdaptor.ViewHolder> {
     }
 
     private class SeedAndSeedlingViewHolder extends CategoryViewHolder{
-
+        RelativeLayout subCategoryLayout;
+        RelativeLayout seedSeedlingTagLayout;
         TextView subCategoryTextView;
+        ImageView subCategoryImageView;
+        TextView seedSeedlingTagTextView;
         ImageView seedSeedlingTagImageView;
 
         private SeedAndSeedlingViewHolder(View currentItemView){
@@ -90,6 +100,8 @@ public class ItemAdaptor extends RecyclerView.Adapter<ItemAdaptor.ViewHolder> {
     }
 
     private class PotAndPlanterViewHolder extends CategoryViewHolder{
+        RelativeLayout sizeTagLayout;
+        ImageView sizeTagImageView;
         TextView sizeTagTextView;
 
         private PotAndPlanterViewHolder(View currentItemView){
@@ -145,46 +157,160 @@ public class ItemAdaptor extends RecyclerView.Adapter<ItemAdaptor.ViewHolder> {
 
         //Category item specific view holder functionality
         if(vh.getClass() == PlantAndTreeViewHolder.class){
-            ((PlantAndTreeViewHolder) vh).subCategoryTextView.setText(currentItem.getPlantSubTag());
+            ((PlantAndTreeViewHolder) vh).subCategoryLayout.setVisibility(View.VISIBLE);
+
+            String subCategory = currentItem.getPlantSubTag();
+            switch (subCategory){
+                case "Evergreen":
+                    ((PlantAndTreeViewHolder) vh).subCategoryImageView.setImageResource(R.drawable.icon_evergreen);
+                    ((PlantAndTreeViewHolder) vh).subCategoryTextView.setText(R.string.tag_evergreen);
+                    break;
+                case "Deciduous":
+                    ((PlantAndTreeViewHolder) vh).subCategoryImageView.setImageResource(R.drawable.icon_deciduous);
+                    ((PlantAndTreeViewHolder) vh).subCategoryTextView.setText(R.string.tag_deciduous);
+                    break;
+                case "Flowering":
+                    ((PlantAndTreeViewHolder) vh).subCategoryImageView.setImageResource(R.drawable.icon_flowering);
+                    ((PlantAndTreeViewHolder) vh).subCategoryTextView.setText(R.string.tag_flowering);
+                    break;
+                case "Fruit":
+                    ((PlantAndTreeViewHolder) vh).subCategoryImageView.setImageResource(R.drawable.icon_fruit);
+                    ((PlantAndTreeViewHolder) vh).subCategoryTextView.setText(R.string.tag_fruit);
+                    break;
+                case "Vegetable":
+                    ((PlantAndTreeViewHolder) vh).subCategoryImageView.setImageResource(R.drawable.icon_vegetable);
+                    ((PlantAndTreeViewHolder) vh).subCategoryTextView.setText(R.string.tag_vegetable);
+                    break;
+                case "Herb":
+                    ((PlantAndTreeViewHolder) vh).subCategoryImageView.setImageResource(R.drawable.icon_herb);
+                    ((PlantAndTreeViewHolder) vh).subCategoryTextView.setText(R.string.tag_herb);
+                    break;
+                case "Succulent":
+                    ((PlantAndTreeViewHolder) vh).subCategoryImageView.setImageResource(R.drawable.icon_succulent);
+                    ((PlantAndTreeViewHolder) vh).subCategoryTextView.setText(R.string.tag_succulent);
+                    break;
+                default:
+                    // Disable the tag if we cannot get the category or category not valid
+                    ((PlantAndTreeViewHolder) vh).subCategoryLayout.setVisibility(View.INVISIBLE);
+            }
 
             if(currentItem.isBestSeller()){
-                ((PlantAndTreeViewHolder) vh).bestSellerImageView.setVisibility(View.VISIBLE);
+                ((PlantAndTreeViewHolder) vh).globalTagLayout.setVisibility(View.VISIBLE);
+                ((PlantAndTreeViewHolder) vh).globalTagImageView.setImageResource(R.drawable.icon_best_seller);
+                ((PlantAndTreeViewHolder) vh).globalTagTextView.setText(R.string.tag_bestSeller);
+            } else if(currentItem.isNewItem()){
+                ((PlantAndTreeViewHolder) vh).globalTagLayout.setVisibility(View.VISIBLE);
+                ((PlantAndTreeViewHolder) vh).globalTagImageView.setImageResource(R.drawable.icon_new_in);
+                ((PlantAndTreeViewHolder) vh).globalTagTextView.setText(R.string.tag_newIn);
+            } else{
+                ((PlantAndTreeViewHolder) vh).globalTagLayout.setVisibility(View.INVISIBLE);
             }
-            if(currentItem.isNewItem()){
-                ((PlantAndTreeViewHolder) vh).newItemImageView.setVisibility(View.VISIBLE);
-            }
+
         }else if(vh.getClass() == SeedAndSeedlingViewHolder.class){
-            ((SeedAndSeedlingViewHolder) vh).subCategoryTextView.setText(currentItem.getPlantSubTag());
 
             if(currentItem.isBestSeller()){
-                ((SeedAndSeedlingViewHolder) vh).bestSellerImageView.setVisibility(View.VISIBLE);
+                ((SeedAndSeedlingViewHolder) vh).globalTagLayout.setVisibility(View.VISIBLE);
+                ((SeedAndSeedlingViewHolder) vh).globalTagImageView.setImageResource(R.drawable.icon_best_seller);
+                ((SeedAndSeedlingViewHolder) vh).globalTagTextView.setText(R.string.tag_bestSeller);
+            } else if(currentItem.isNewItem()){
+                ((SeedAndSeedlingViewHolder) vh).globalTagLayout.setVisibility(View.VISIBLE);
+                ((SeedAndSeedlingViewHolder) vh).globalTagImageView.setImageResource(R.drawable.icon_new_in);
+                ((SeedAndSeedlingViewHolder) vh).globalTagTextView.setText(R.string.tag_newIn);
+            } else{
+                ((SeedAndSeedlingViewHolder) vh).globalTagLayout.setVisibility(View.INVISIBLE);
             }
-            if(currentItem.isNewItem()){
-                ((SeedAndSeedlingViewHolder) vh).newItemImageView.setVisibility(View.VISIBLE);
+
+            String subCategory = currentItem.getPlantSubTag();
+            switch (subCategory) {
+                case "Evergreen":
+                    ((SeedAndSeedlingViewHolder) vh).subCategoryImageView.setImageResource(R.drawable.icon_evergreen);
+                    ((SeedAndSeedlingViewHolder) vh).subCategoryTextView.setText(R.string.tag_evergreen);
+                    break;
+                case "Deciduous":
+                    ((SeedAndSeedlingViewHolder) vh).subCategoryImageView.setImageResource(R.drawable.icon_deciduous);
+                    ((SeedAndSeedlingViewHolder) vh).subCategoryTextView.setText(R.string.tag_deciduous);
+                    break;
+                case "Flowering":
+                    ((SeedAndSeedlingViewHolder) vh).subCategoryImageView.setImageResource(R.drawable.icon_flowering);
+                    ((SeedAndSeedlingViewHolder) vh).subCategoryTextView.setText(R.string.tag_flowering);
+                    break;
+                case "Fruit":
+                    ((SeedAndSeedlingViewHolder) vh).subCategoryImageView.setImageResource(R.drawable.icon_fruit);
+                    ((SeedAndSeedlingViewHolder) vh).subCategoryTextView.setText(R.string.tag_fruit);
+                    break;
+                case "Vegetable":
+                    ((SeedAndSeedlingViewHolder) vh).subCategoryImageView.setImageResource(R.drawable.icon_vegetable);
+                    ((SeedAndSeedlingViewHolder) vh).subCategoryTextView.setText(R.string.tag_vegetable);
+                    break;
+                case "Herb":
+                    ((SeedAndSeedlingViewHolder) vh).subCategoryImageView.setImageResource(R.drawable.icon_herb);
+                    ((SeedAndSeedlingViewHolder) vh).subCategoryTextView.setText(R.string.tag_herb);
+                    break;
+                case "Succulent":
+                    ((SeedAndSeedlingViewHolder) vh).subCategoryImageView.setImageResource(R.drawable.icon_succulent);
+                    ((SeedAndSeedlingViewHolder) vh).subCategoryTextView.setText(R.string.tag_succulent);
+                    break;
+                default:
+                    // Disable the tag if we cannot get the category or category not valid
+                    ((SeedAndSeedlingViewHolder) vh).subCategoryLayout.setVisibility(View.INVISIBLE);
             }
+
+            ((SeedAndSeedlingViewHolder) vh).seedSeedlingTagLayout.setVisibility(View.VISIBLE);
             if(currentItem.getSeedSubTag().equals("Seed")){
-                //set image view with seed tag image
+                ((SeedAndSeedlingViewHolder) vh).seedSeedlingTagImageView.setImageResource(R.drawable.icon_seed);
+                ((SeedAndSeedlingViewHolder) vh).seedSeedlingTagTextView.setText(R.string.tag_seed);
             } else if (currentItem.getSeedSubTag().equals("Seedling")) {
-                //set image view with seedling tag image
+                ((SeedAndSeedlingViewHolder) vh).seedSeedlingTagImageView.setImageResource(R.drawable.icon_seedling);
+                ((SeedAndSeedlingViewHolder) vh).seedSeedlingTagTextView.setText(R.string.tag_seedling);
+            } else{
+                ((SeedAndSeedlingViewHolder) vh).seedSeedlingTagLayout.setVisibility(View.INVISIBLE);
             }
-        }else if(vh.getClass() == PotAndPlanterViewHolder.class){
-            ((PotAndPlanterViewHolder) vh).sizeTagTextView.setText(currentItem.getSize());
 
+        }else if(vh.getClass() == PotAndPlanterViewHolder.class){
             if(currentItem.isBestSeller()){
-                ((PotAndPlanterViewHolder) vh).bestSellerImageView.setVisibility(View.VISIBLE);
+                ((PotAndPlanterViewHolder) vh).globalTagLayout.setVisibility(View.VISIBLE);
+                ((PotAndPlanterViewHolder) vh).globalTagImageView.setImageResource(R.drawable.icon_best_seller);
+                ((PotAndPlanterViewHolder) vh).globalTagTextView.setText(R.string.tag_bestSeller);
+            } else if(currentItem.isNewItem()){
+                ((PotAndPlanterViewHolder) vh).globalTagLayout.setVisibility(View.VISIBLE);
+                ((PotAndPlanterViewHolder) vh).globalTagImageView.setImageResource(R.drawable.icon_new_in);
+                ((PotAndPlanterViewHolder) vh).globalTagTextView.setText(R.string.tag_newIn);
+            } else{
+                ((PotAndPlanterViewHolder) vh).globalTagLayout.setVisibility(View.INVISIBLE);
             }
-            if(currentItem.isNewItem()){
-                ((PotAndPlanterViewHolder) vh).newItemImageView.setVisibility(View.VISIBLE);
+
+            ((PotAndPlanterViewHolder) vh).sizeTagLayout.setVisibility(View.VISIBLE);
+            String size = currentItem.getSize();
+            switch (size){
+                case "Small":
+                    ((PotAndPlanterViewHolder) vh).sizeTagImageView.setImageResource(R.drawable.icon_small_pot);
+                    ((PotAndPlanterViewHolder) vh).sizeTagTextView.setText(R.string.tag_smallPot);
+                    break;
+                case "Medium":
+                    ((PotAndPlanterViewHolder) vh).sizeTagImageView.setImageResource(R.drawable.icon_medium_pot);
+                    ((PotAndPlanterViewHolder) vh).sizeTagTextView.setText(R.string.tag_mediumPot);
+                    break;
+                case "Large":
+                    ((PotAndPlanterViewHolder) vh).sizeTagImageView.setImageResource(R.drawable.icon_large_pot);
+                    ((PotAndPlanterViewHolder) vh).sizeTagTextView.setText(R.string.tag_largePot);
+                    break;
+                default:
+                    ((PotAndPlanterViewHolder) vh).sizeTagLayout.setVisibility(View.INVISIBLE);
             }
+
         }else if(vh.getClass() == CategoryViewHolder.class){ //Plant care and decor category
             if(currentItem.isBestSeller()){
-                ((CategoryViewHolder) vh).bestSellerImageView.setVisibility(View.VISIBLE);
-            }
-            if(currentItem.isNewItem()){
-                ((CategoryViewHolder) vh).newItemImageView.setVisibility(View.VISIBLE);
+                ((CategoryViewHolder) vh).globalTagLayout.setVisibility(View.VISIBLE);
+                ((CategoryViewHolder) vh).globalTagImageView.setImageResource(R.drawable.icon_best_seller);
+                ((CategoryViewHolder) vh).globalTagTextView.setText(R.string.tag_bestSeller);
+            } else if(currentItem.isNewItem()){
+                ((CategoryViewHolder) vh).globalTagLayout.setVisibility(View.VISIBLE);
+                ((CategoryViewHolder) vh).globalTagImageView.setImageResource(R.drawable.icon_new_in);
+                ((CategoryViewHolder) vh).globalTagTextView.setText(R.string.tag_newIn);
+            } else{
+                ((CategoryViewHolder) vh).globalTagLayout.setVisibility(View.INVISIBLE);
             }
         }
-
     }
 
     @Override
